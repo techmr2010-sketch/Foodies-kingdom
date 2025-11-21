7e336f4f3b690f2f02c2b3710608aaf9200f556e
Tech Mr. <techmr.2010@gmail.com>
2025-11-21 11:30:02 +0000

    add the photho of momos veg fry in section of momos veg fry

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'plus.unsplash.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
