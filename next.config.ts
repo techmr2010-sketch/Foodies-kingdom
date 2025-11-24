05252531e0dac044f9f95a1fa0add495f5bc36b9
Tech Mr. <techmr.2010@gmail.com>
2025-11-24 08:54:14 +0000

    add thi photo at the momos fry

--- next.config.ts
+++ next.config.ts
@@ -33,6 +33,12 @@ const nextConfig: NextConfig = {
         hostname: 'plus.unsplash.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
